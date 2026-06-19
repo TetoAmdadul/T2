b7d388baa88654b571ad232623ccf724132c2a33
Teto Amdadul <ridhomteto@gmail.com>
2026-06-19 18:59:08 +0300

    cleaner code

--- Python/fizzBuzzNumber.py
+++ Python/fizzBuzzNumber.py
@@ -1,12 +1,10 @@
 print('Enter an Integer:')
 n = int(input())
-if n%3 == 0 and n%5 ==0:
-      print('Fizz Buzz')
+if n % 3 == 0 and n % 5 ==0:
+    print('Fizz Buzz')
 elif n%3 == 0:
     print('Fizz')
 elif n%5 == 0:
-     print('Buzz')
-#elif n%3 == 0 or n%5 ==0:
- #     print('Fizz Buzz')
+    print('Buzz')
 else:
     print(n)
